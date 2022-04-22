22e399e87ac091176bc7acc9b6c8c79d504dd728
phi <nguyentruongphi15032003@gmail.com>
2022-04-22 18:36:54 +0700

    request permission

---
+++ app/src/androidTest/java/com/example/intentcameraandgallery/ActivityMainBinding.java
@@ -0,0 +1,4 @@
+package com.example.intentcameraandgallery;
+
+public class ActivityMainBinding {
+}
--- app/src/main/java/com/example/intentcameraandgallery/MainActivity.java
+++ app/src/main/java/com/example/intentcameraandgallery/MainActivity.java
@@ -1,14 +1,60 @@
 package com.example.intentcameraandgallery;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.example.intentcameraandgallery.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding mBiding;
+    int REQUEST_CODE_CAMERA = 123;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBiding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBiding.getRoot());
+
+        mBiding.buttonCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.CAMERA)){
+                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                        builder.setTitle("Bạn cần cấp quyền cho camera");
+                        builder.setPositiveButton("Cài đặt", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Intent intent = new Intent(Settings.ACTION_APPLICATION_SETTINGS);
+                                Uri uri = Uri.fromParts("package", getPackageName(), null);
+                                intent.setData(uri);
+                                startActivity(intent);
+
+                            }
+                        });
+                        builder.show();
+                    } else {
+                        ActivityCompat.requestPermissions(
+                                MainActivity.this,
+                                new String[]{Manifest.permission.CAMERA},
+                                REQUEST_CODE_CAMERA
+                        );
+                    }
+                }
+
+            }
+        });
     }
 }
